cb790f2f37e2f6279eb88e940d6e277cebd4cc10
Taha Bouhsine <45195299+skywolfmo@users.noreply.github.com>
2024-03-02 13:52:06 +0000

    prep

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set default values for source and destination folders
-unzip /content/rice-image-dataset.zip ./dataset
+unzip /content/rice-image-dataset.zip -d ./dataset
 source_folder="./dataset"
 destination_folder="/content/beyond-the-jupyter-notebook/data/processed"
 
