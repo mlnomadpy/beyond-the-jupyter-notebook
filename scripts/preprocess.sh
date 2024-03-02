1ae2f030cffc7e7ab0a32579934a67b3dab12775
Taha Bouhsine <45195299+skywolfmo@users.noreply.github.com>
2024-03-02 13:50:31 +0000

    prep

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Set default values for source and destination folders
-source_folder="/kaggle/input/rice-image-dataset/Rice_Image_Dataset"
-destination_folder="/kaggle/working/beyond-the-jupyter-notebook/data/processed"
+unzip /content/rice-image-dataset.zip ./dataset
+source_folder="./dataset"
+destination_folder="/content/beyond-the-jupyter-notebook/data/processed"
 
 # Run the Python script to copy folders
-python /kaggle/working/beyond-the-jupyter-notebook/src/utils/preprocessing/preprocess_data.py "$source_folder" "$destination_folder"
+python /content/beyond-the-jupyter-notebook/src/utils/preprocessing/preprocess_data.py "$source_folder" "$destination_folder"
